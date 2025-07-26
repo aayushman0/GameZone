38c94e305d0a2372791608e02e292ec3683c849b
aayushman0 <aayushman.maharjan0@outlook.com>
2025-07-26 18:51:26 +0545

    changes to summary

--- GUI/summary.py
+++ GUI/summary.py
@@ -12,23 +12,31 @@ class SummaryFrame(Frame):
     def tk_vars(self):
         self.total_expense = StringVar()
         self.total_income = StringVar()
+        self.remaining_credit = StringVar()
+        self.current_capital = StringVar()
 
     def main(self):
         ttk.Label(self.add_frame, text="Date:").grid(row=self.i, column=0, sticky="e", pady=10)
         self.i
-        ttk.Label(self.add_frame, text="Expense:").grid(row=self.i, column=0, sticky="e", pady=10)
-        ttk.Label(self.add_frame, text="Income:").grid(row=self.i, column=0, sticky="e", pady=10)
+        ttk.Label(self.list_frame, text="Total Expense:").grid(row=self.i, column=0, sticky="e", pady=10)
+        ttk.Label(self.list_frame, text="Total Income:").grid(row=self.i, column=0, sticky="e", pady=10)
+        self.i
+        ttk.Label(self.list_frame, text="Remaining Credit:").grid(row=self.i, column=0, sticky="e", pady=10)
+        ttk.Label(self.list_frame, text="Current Capital:").grid(row=self.i, column=0, sticky="e", pady=10)
         self.i = 0
         date_frame = ttk.Frame(self.add_frame)
-        date_frame.grid(row=self.i, column=1, sticky="w")
+        date_frame.grid(row=self.i, column=1, sticky="ew", pady=10)
         date_entry(self, date_frame)
         ttk.Button(
             self.add_frame, text="Filter", style="Confirm.TButton", command=self.update_total
         ).grid(
-            row=self.i, column=0, columnspan=2, sticky="ew", ipady=10
+            row=self.i, column=1, sticky="ew", ipady=10, padx=(20, 50)
         )
-        ttk.Entry(self.add_frame, textvariable=self.total_expense, state="readonly").grid(row=self.i, column=1, sticky="ew")
-        ttk.Entry(self.add_frame, textvariable=self.total_income, state="readonly").grid(row=self.i, column=1, sticky="ew")
+        ttk.Entry(self.list_frame, textvariable=self.total_expense, state="readonly").grid(row=self.i, column=1, sticky="ew")
+        ttk.Entry(self.list_frame, textvariable=self.total_income, state="readonly").grid(row=self.i, column=1, sticky="ew")
+        ttk.Separator(self.list_frame).grid(row=self.i, column=0, columnspan=2, sticky="ew", pady=10)
+        ttk.Entry(self.list_frame, textvariable=self.remaining_credit, state="readonly").grid(row=self.i, column=1, sticky="ew")
+        ttk.Entry(self.list_frame, textvariable=self.current_capital, state="readonly").grid(row=self.i, column=1, sticky="ew")
 
     def update_total(self):
         year = self.year.get()
@@ -48,3 +56,5 @@ class SummaryFrame(Frame):
         self.day.set(current_time.day)
         self.total_expense.set(summary.total_expense())
         self.total_income.set(summary.total_income())
+        self.remaining_credit.set(summary.remaining_credit())
+        self.current_capital.set(summary.current_capital())
--- db/orm/summary.py
+++ db/orm/summary.py
@@ -1,5 +1,5 @@
 from sqlalchemy import func
-from db.models import session, Income, Expense
+from db.models import session, Income, Expense, Credit
 from datetime import date
 
 
@@ -21,3 +21,20 @@ def total_income(from_: date | None = None, to_: date | None = None) -> float:
         incomes = incomes.filter(func.DATE(Income.date) <= to_)
     total = sum(income.price for income in incomes)
     return total
+
+
+def remaining_credit() -> float:
+    credits = session.query(Credit)
+    remaining_credit = sum(credit.current_amount for credit in credits)
+    return remaining_credit
+
+
+def current_capital() -> float:
+    incomes = session.query(Income)
+    expenses = session.query(Expense)
+    credits = session.query(Credit)
+    total_income = sum(income.price for income in incomes)
+    total_expense = sum(expense.cost for expense in expenses)
+    total_current_credit = sum(credit.initial_amount for credit in credits)
+    current_amount = total_income - (total_expense - total_current_credit)
+    return current_amount
