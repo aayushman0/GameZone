d49df6cd6d82e69031f8325954349b44b693bca5
aayushman0 <aayushman.maharjan0@outlook.com>
2025-08-08 16:51:14 +0545

    handle error due to disabled income

--- db/orm/summary.py
+++ db/orm/summary.py
@@ -4,38 +4,34 @@ from datetime import date
 
 
 def total_expense(from_: date | None = None, to_: date | None = None) -> float:
-    expenses = session.query(Expense)
+    expenses = session.query(func.sum(Expense.cost)).filter(Expense.is_enabled.is_(True))
     if from_:
         expenses = expenses.filter(func.DATE(Expense.date) >= from_)
     if to_:
         expenses = expenses.filter(func.DATE(Expense.date) <= to_)
-    total = sum(expense.cost for expense in expenses)
+    total = expenses.scalar()
     return total
 
 
 def total_income(from_: date | None = None, to_: date | None = None) -> float:
-    incomes = session.query(Income)
+    incomes = session.query(func.sum(Income.price)).filter(Income.is_enabled.is_(True))
     if from_:
         incomes = incomes.filter(func.DATE(Income.date) >= from_)
     if to_:
         incomes = incomes.filter(func.DATE(Income.date) <= to_)
-    total = sum(income.price for income in incomes)
+    total = incomes.scalar()
     return total
 
 
 def remaining_credit() -> float:
-    credits = session.query(Credit)
-    remaining_credit = sum(credit.current_amount for credit in credits)
+    remaining_credit = session.query(func.sum(Credit.current_amount)).scalar()
     return remaining_credit
 
 
 def current_capital() -> float:
-    incomes = session.query(Income)
-    expenses = session.query(Expense)
-    credits = session.query(Credit)
-    total_income = sum(income.price for income in incomes)
-    total_expense = sum(expense.cost for expense in expenses)
-    paid_credit = sum((credit.initial_amount - credit.current_amount) for credit in credits)
-    total_current_credit = sum(credit.initial_amount for credit in credits)
+    total_income = session.query(func.sum(Income.price)).filter(Income.is_enabled.is_(True)).scalar()
+    total_expense = session.query(func.sum(Expense.cost)).filter(Expense.is_enabled.is_(True)).scalar()
+    total_current_credit = session.query(func.sum(Credit.initial_amount)).scalar()
+    paid_credit = total_current_credit - session.query(func.sum(Credit.current_amount)).scalar()
     current_amount = total_income - (total_expense - total_current_credit) - paid_credit
     return current_amount
